85138724fb1e134e8858221e04a2e348a3c2b280
FuNk <robesckeydangol2006@gmail.com>
2023-05-17 11:37:52 +0545

    updated main code and html

--- main.py
+++ main.py
@@ -11,83 +11,176 @@ from src.boltz import *
 app = Flask(__name__)
 CORS(app)
 
-def progressAdddef():
-    file_data = None
+'''
 
-    with open("user_progress.json","r") as json_file:
-        file_data = json.load(json_file)
-        json_file.close()
+! parses token from client
 
-    with open("user_progress.json","w") as json_file:
+'''
+def parse_token(data):
+    data = data.decode()
+    data = data.replace('"','')
+    return data
+
+
+'''
+
+! generates default value for client and generates token
+
+'''
+def progress_add_def():
+    fileData = None
+
+    with open("user_progress.json","r") as jsonFile:
+        fileData = json.load(jsonFile)
+        jsonFile.close()
+
+    with open("user_progress.json","w") as jsonFile:
+        
+        # generating file token 
         token = secrets.token_hex(8)
+
         detail = {"total_progress": 0, "current_song": "Null", "current_song_progress":0,"is_complete": False}
-        file_data.update({token:detail})
-        json.dump(file_data,json_file)
-        json_file.close()
+        fileData.update({token:detail})
+        json.dump(fileData,jsonFile)
+        jsonFile.close()
+
     return token
 
+
+'''
+
+! used for progress updating
+
+'''
 @app.route('/get', methods= ['POST'])
-def example_route():
-    token = request.data
-    token = token.decode()
-    token = token.replace('"','')
-    with open("user_progress.json","r") as json_file:
-        file_data = json.load(json_file)
-        json_file.close()
-    tprogress = file_data[token]["total_progress"]
-    csong = file_data[token]["current_song"]
-    iscomplete = file_data[token]["is_complete"]
+def progress_route():
+
+    # parsing token from client
+    token = parse_token(request.data)
+
+    with open("user_progress.json","r") as jsonFile:
+        fileData = json.load(jsonFile)
+        jsonFile.close()
+    
+    '''
+
+    * totalProgress = Total progress of conversion in %
+    * currentSong = current song being converted
+    * isComplete = true or false when the total conversion is completed
+
+    '''
+
+    totalProgress = fileData[token]["total_progress"]
+    currentSong = fileData[token]["current_song"]
+    isComplete = fileData[token]["is_complete"]
 
     data = {
-        "total_progress": str(tprogress),
-        "current_song": csong,
-        "is_complete": iscomplete,
+        "total_progress": str(totalProgress),
+        "current_song": currentSong,
+        "is_complete": isComplete,
     }
+
     return jsonify(data)
 
+
+'''
+
+! for home screen
+
+'''
 @app.route('/')
 def home():
+
+    '''
+    * link = spotify playlist link
+    * fileToken = token generated for each conversion
+    '''
+
     link = str(request.args.get("linkBox"))
+
+    # checking if link provided is valid
     if(link.startswith('https://open.spotify.com/')):
-        file_token = progressAdddef()
-        thread = threading.Thread(target=downloadPl, args=(link, False, file_token,))
-        thread.start()
-        return redirect(url_for("download_page", Token = file_token))
+
+        fileToken = progress_add_def()
+
+        conversionThread = threading.Thread(target=downloadPl, args=(link, fileToken,))
+        conversionThread.start()
+
+        return redirect(url_for("download_page", token = fileToken))
     else:
         return render_template("upload.html")
 
-@app.route('/<Token>')
-def download_page(Token):
-    return render_template("download.html",file_token = Token, server_ip = SERVER_LINK)
 
+'''
+
+! for download page
+
+'''
+@app.route('/<token>')
+def download_page(token):
+    return render_template("download.html",file_token = token, server_ip = SERVER_LINK)
+
+'''
+
+! provides file to the client when redirected
+
+'''
 @app.route('/download/<token>')
 def download_zip(token):
     return send_file(f"{DOWNLOADS_FOLDER}/{token}/{token}.zip")
 
+'''
+
+! for search page
+
+'''
 @app.route('/search')
 def search():
-    keywords = str(request.args.get("linkBox"))
-    search_type = str(request.args.get("type"))
-    results = spotify_search(keywords, search_type)
+    '''
+
+    * keyWords = search text
+    * searchType = which to search for, either album, track or playlist
+    * results = list of dict full of results and data
+
+    '''
+
+    keyWords = str(request.args.get("linkBox"))
+    searchType = str(request.args.get("type"))
+
+    # searching for the results
+    results = spotify_search(keyWords, searchType)
+
+    # checking if there is no result
     if(str(results) == "None"):
+        
         return render_template("search.html")
     else:
-        return render_template("result.html", items = results, search_t = search_type, server_ip = SERVER_LINK)
 
-@app.route('/res/<res>')
-def result(res):
-    res = res.split(',')
-    link = 'https://open.spotify.com/' + res[0] + '/' + res[1]
+        return render_template("result.html", results = results, search_type = searchType, server_ip = SERVER_LINK)
+
+'''
+! for redirecting to conversion page
+'''
+
+@app.route('/res/<raw>')
+def result(raw):
+
+    # parsing and generating spotify link
+    raw = raw.split(',')
+    link = 'https://open.spotify.com/' + raw[0] + '/' + raw[1]
     
-    file_token = progressAdddef()
-    thread = threading.Thread(target=downloadPl, args=(link, False, file_token,))
-    thread.start()
-    return redirect(url_for("download_page", Token = file_token))
+    fileToken = progress_add_def()
 
-    
+    conversionThread = threading.Thread(target=downloadPl, args=(link, fileToken,))
+    conversionThread.start()
+
+    return redirect(url_for("download_page", token = fileToken))
 
 
-    
+'''
 
+! runs server on execution
+
+'''
 if __name__ == '__main__':
     app.run(host=SERVER_IP, port=SERVER_PORT, debug=True)
--- src/boltz.py
+++ src/boltz.py
@@ -1,4 +1,5 @@
-from src.imports import *
+import os
+
 from src.boltzUtil import *
 from src.constants import VERSION, DOWNLOADS_FOLDER
 from src.spotify_dl import *
@@ -6,14 +7,18 @@ from src.spotify_dl import *
 '''
 Generates File System For Song Download
 '''
-def generateFilesystem( generateSeperatefolder):
+def generate_file_system(generateSeperatefolder):
     if(generateSeperatefolder == True):
         try:
-            boltzUtil.logHeader("Attempting to generate file system")
+            logHeader("Attempting to generate file system")
+
             os.mkdir(DOWNLOADS_FOLDER)
+
             logHeader("Sucessfully created file system")
+
         except Exception as e:
-            logError("Failed to create downloads folder")
+
+            pass
     else:
         pass
 
@@ -21,19 +26,12 @@ def generateFilesystem( generateSeperatefolder):
 Download Playlist Function
 '''
 
-def downloadPl( playListlink, isZip, token):
+def downloadPl( playListlink, token):
     playListlink = playListlink
-    isZip = isZip
     generateFilesystem(True)
     spotify_dl(playListlink, token)
 
 
-    
-if __name__ == '__main__':
-    downloadPl('https://open.spotify.com/playlist/2y0Epzc7sqZGf8aDXGxZTx?si=6d166e8969574bed',False)
-
-
-            
 
 
 
