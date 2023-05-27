c7f43ba41cd06fc55c0df7b2faaafa26ef686817
FuNk <robesckeydangol2006@gmail.com>
2023-05-28 03:58:30 +0545

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -14,6 +14,6 @@ else:
 
 DOWNLOAD_LIST = "download_list.log"
 DOWNLOADS_FOLDER = "./downloads"
-CLIENT_ID = '0797e2fdf87b42ca8469beae2587bae4'
-CLIENT_SECRET = '19fd3eb103e7482587c186b760b8f3c3'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
 
